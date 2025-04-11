5811f16e2f3531ebd4ea2eb4af41fd97314cf308
Shahbaz Liaquat <102903848+shahbazliaquat0@users.noreply.github.com>
2025-04-11 18:08:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -38,11 +38,49 @@
     // });
 
     // fade out
+    // $(document).ready(function(){
+    //     $("button").click(function(){
+    //         $(".first").fadeOut(2000);
+    //         $(".second").fadeOut(1000);
+    //         $(".third").fadeOut(500);
+    //         $(".fourth").fadeOut("fast");
+    //     });
+    // });
+
+    // fade toggle 
+    // $(document).ready(function(){
+    //     $("button").click(function(){
+    //         $("div").fadeToggle(); 
+    //     });
+    // });
+
+    // $(document).ready(function(){
+    //     $("button").click(function(){
+    //         $(".first").fadeToggle(2000);
+    //         $(".second").fadeToggle(1000);
+    //         $(".third").fadeToggle(500);
+    //         $(".fourth").fadeToggle("fast");
+    //     });
+    // });
+
+
+    // slide in and slide out jQuery 
+
+
+    // $(document).ready(function(){
+    //     $("button").click(function(){
+    //         $("div").slideDown();
+    //     });
+    // });
+
+    // $(document).ready(function(){
+    //     $("button").click(function(){
+    //         $("div").slideUp();
+    //     });
+    // });
+
     $(document).ready(function(){
         $("button").click(function(){
-            $(".first").fadeOut(2000);
-            $(".second").fadeOut(1000);
-            $(".third").fadeOut(500);
-            $(".fourth").fadeOut("fast");
+            $("div").slideToggle();
         });
     });
